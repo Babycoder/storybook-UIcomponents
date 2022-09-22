49adffcfeb9edbf0fd22474e7e8acc635e417f47
ghazali <aymenghazali1999@gmail.com>
2022-09-22 15:07:53 +0100

    button-secondary

--- src/Button.tsx
+++ src/Button.tsx
@@ -4,7 +4,6 @@ import React, {HTMLAttributes, ReactNode} from 'react'
 export interface Props extends HTMLAttributes<HTMLButtonElement>
 {
     children : ReactNode;
-
     type : 'primary' | 'secondary' | 'inline';
 }
 
--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -1,18 +1,46 @@
 import React from 'react';
 import {Meta, Story} from '@storybook/react'
 import {Props}  from "../src/Button"
+import {action} from '@storybook/addon-actions'
+
 import Button from '../src/Button'
 
 const meta:Meta = {
     title : 'Button',
     component: Button,
+    argTypes: {
+        
+        onClick: { action :'clicked' },
+
+        children : {
+            defaultValue : 'Click Me'
+        }
+    }
 }
 
 export default meta;
 
 const Template: Story<Props> = (args) => <Button {...args} />
 
+export const Default = Template.bind({});
 
+export const Primary = Template.bind({});
+
+export const Secondary = Template.bind({});
+
+
+Primary.args ={
+    type : 'primary',
+    children : 'Buy daba',
+    onClick: action('Primary click'),
+}
+
+Secondary.args ={
+    type : 'secondary',
+    children : 'Buy now',
+    onClick: action('Secondary click'),
+
+}
 
 /*export const Default = () => <Button type="primary">Click Me</Button>
 
